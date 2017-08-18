6c5989c26fcd92700a05298fa8d222b669622a49
Ellen Bartling <ellen@emergentorder.com>
2017-08-18 15:00:26 -0500

    Swap out social icons for buttons, and make some line-spacing adjustments

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,8 +5,9 @@
       <p>Enter your information, copy, paste, and you're good to go.</p>
     </div>
     <div class="row">
-      <div class="col-12 form-wrapper">
+      <div class="col-12 col-md-5">
         <h5>Enter your information here:</h5>
+        <div class="form-wrapper">
         <div class="form-group">
           <label>Image:</label>
           <input class="form-control" v-model="image">
@@ -40,7 +41,8 @@
           <input class="form-control" v-model="social[key]">
         </div>
       </div>
-      <div class="col signature-renderer">
+      </div>
+      <div class="col-12 col-md-6 ml-auto signature-renderer">
         <h5>Copy & paste from here:</h5>
         <div id="signature" style="padding: 10px">
           <div id="copy-this" v-html="signature"></div>
@@ -66,12 +68,14 @@ export default {
       name: 'Your Name Here',
       title: 'Your title here',
       department: 'You Can List A Department',
-      image: 'https://en.gravatar.com/avatar/c5944e489306f210199feb557d4693a7?s=200',
+      // image: 'https://en.gravatar.com/avatar/c5944e489306f210199feb557d4693a7?s=200',
+      image: '',
       contact: {
         email: 'your.email@address.com',
         phone: '555-555-5555',
         cell: '555-555-5555',
-        profile: 'http://www.pacificlegal.com/staff/this-feature-coming-soon'
+        profile: 'http://www.pacificlegal.com/staff/this-feature-coming-soon',
+        fax: '(916) 419-7111'
       },
       social: {
         facebook: 'http://www.facebook.com/PacificLegalFoundation/',
@@ -80,7 +84,8 @@ export default {
       },
       vars: {
         cobalt: '#0053BC',
-        yellow: '#F4B73E'
+        yellow: '#F4B73E',
+        navy: '#20334B'
       },
       successtext: ''
     }
@@ -117,6 +122,26 @@ export default {
       // <span style="font-color:black">phone:</span> <span><a style="color: ${app.vars.cobalt}; font-weight: bold" href="tel:${app.phone}">${app.phone}</a></span>
       // <br/ >
     },
+    photo: function () {
+      var app = this
+      if (app.image) {
+        return `<mj-column width="100px" padding="0px"><mj-image padding="0px 20px 0px 0px" src="${app.image}"></mj-image></mj-column>`
+      } else {
+        return ''
+      }
+    },
+    socialAll: function () {
+      var app = this
+      var obj = this.social
+      var r = ""
+      for (var s in obj) {
+        if(obj.hasOwnProperty(s) && obj[s]) {
+          r += `<mj-column><mj-button href="${obj[s]}"><mj-image src="https://github.com/Emergent-Order/plf-email-signatures/blob/master/assets/${s}-icon.png?raw=true"></mj-image></mj-button></mj-column>`
+        }
+      }
+
+      return r
+    },
     signature: function () {
       var app = this
 
@@ -133,17 +158,21 @@ export default {
             <mj-container align="left">
               <mj-section text-align="left" padding="0px" >
                 <mj-group style="width: 100%">
-                  <mj-column width="100px" padding="0px">
-                    <mj-image padding="0px 20px 0px 0px" src="${app.image}"></mj-image>
-                  </mj-column>
+                    ${app.photo}
                   <mj-column >
+                    <mj-text
+                      font-size="18px"
+                      align="left"
+                      padding="10px 0px 0px 0px"
+                      color="${app.vars.navy}"
+                      >
+                      <span style="font-weight:bold">${app.name} | </span> <span>${app.title}</span>
+                    </mj-text>
                     <mj-text
                       font-size="14px"
                       align="left"
-                      padding="10px 0px"
+                      padding="0px 0px 10px 0px"
                       >
-                      <span style="font-weight:bold">${app.name} | </span> <span>${app.title}</span>
-                      <br/ >
                       <span style="font-style:italic">${app.department}</span>
                       <br/>
                       <span><a style="color: ${app.vars.cobalt}; font-weight: bold" href="http://www.pacificlegal.org">Pacific Legal Foundation</a></span>
@@ -152,9 +181,22 @@ export default {
                       font-size="14px"
                       align="left"
                       padding="0px 0px 10px"
+                      line-height="20px"
                       >
                       ${app.contactInfo}
                     </mj-text>
+                    <mj-text
+                      font-size="14px"
+                      align="left"
+                      padding="0px 0px 10px"
+                      line-height="20px"
+                      >
+                      <span>930 G Street | Sacramento, CA 95814</span>
+                    </mj-text>
+                    <mj-group>
+                      <mj-column>
+
+
                       <mj-social
                         text-mode="false"
                         align="left"
@@ -189,8 +231,6 @@ export default {
       $(firstTable).css('width', '100%')
       $(firstTable).attr('align', '')
 
-      console.log($('table')[0])
-
       return $.html()
     }
   },
@@ -199,7 +239,6 @@ export default {
 
       if (document.selection) {
           var content = document.getElementsTagName('table')[0]
-          console.log(content)
           var range = document.body.createTextRange()
           range.moveToElementText(content)
           range.select();
@@ -209,7 +248,6 @@ export default {
           document.execCommand('Copy');
       } else if (window.getSelection) {
           var content = document.getElementsByTagName('table')[0]
-          console.log(content)
           var range = document.createRange();
           range.selectNode(content);
           window.getSelection().removeAllRanges();
@@ -228,6 +266,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass">
+  body
+    background-color: #F2F2F2
   .container
     margin-top: 40px
     margin-bottom: 40px
@@ -236,7 +276,7 @@ export default {
   .form-control
     border-radius: 0px
   #signature
-    border: 1px solid lighten(#4A4A4A, 55%)
+    background-color: white
   .btn
     background-color: #002997
     border-radius: 0px
@@ -252,7 +292,7 @@ export default {
     margin-bottom: 1rem
   .form-wrapper
     background-color: #F2F2F2
-    padding: 30px
+    // padding: 30px
   .section-break
     margin-top: 50px
   .signature-renderer
